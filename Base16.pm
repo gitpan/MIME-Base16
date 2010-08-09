5a34447bdc98e6aee50187b527f5fa3e37b53d7e
- <stefanos@cpan.org>
2010-08-09 04:19:11 +0000

    Import of STEFANOS/MIME-Base16-1.0 from CPAN.
    gitpan-cpan-distribution: MIME-Base16
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         STEFANOS/MIME-Base16-1.0.tar.gz
    gitpan-cpan-author:       STEFANOS
    gitpan-cpan-maturity:     released

---
+++ Base16.pm
@@ -0,0 +1,66 @@
+package MIME::Base16;
+
+require 5.005_62;
+use strict;
+use warnings;
+
+use vars qw( $VERSION );
+$VERSION = '1.0';
+
+sub import {
+	*encode = \&encode_base16;
+	*decode = \&decode_base16;
+}
+
+sub decode_base16 {
+	my $arg = shift;
+	my $ret = "";
+	for(my $i=0;$i<length($arg);$i+=2){
+		my $tmp = substr($arg,$i,2);
+		my $int = hex($tmp);
+		$ret .= chr ($int);
+	}
+	return $ret;
+}
+
+sub encode_base16 {
+	my $arg = shift;
+	my $ret = "";
+	for(my $i=0;$i<length($arg);$i+=1){
+		my $tmp = ord(substr($arg,$i,1));
+		$ret .= sprintf "%x", $tmp;
+	}
+	return $ret;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+MIME::Base16 - Base16 encoder / decoder
+
+=head1 SYNOPSIS
+
+      use MIME::Base16; 
+
+      $encoded = MIME::Base16::encode($data);
+      $decoded = MIME::Base16::decode($encoded);
+
+=head1 DESCRIPTION
+
+Encode data similar way like MIME::Base64 does.
+
+=head1 EXPORT
+
+NOTHING
+
+=head1 AUTHOR
+
+Stefan Gipper <stefanos@cpan.org>, http://www.coder-world.de/
+
+=head1 SEE ALSO
+
+perl(1), MIME::Base64(3pm).
+
+=cut
